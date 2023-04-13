0bc1496ba4fa942022d45ee64ae8b9147df37f37
vicky.vignesh <vignesh.santhapeta@gmail.com>
2023-04-13 21:38:50 +0530

    birthday

---
+++ index.js
@@ -0,0 +1,32 @@
+document.getElementById("mar").onclick=function(){
+    document.getElementById("demo").style.backgroundImage="none"
+
+    document.getElementById("hello").style.backgroundColor="rgb(72,6,7)"
+    setTimeout(()=>{
+        
+        document.getElementById('demo').style.backgroundImage=" linear-gradient(rgb(0, 255, 106),rgb(241, 163, 176),purple,black)";
+
+
+    },2000)
+        setTimeout(()=>{
+        document.getElementById('cracker').style.backgroundImage="url('./face/animated/shorts.gif')";
+
+    },3000)
+    setTimeout(()=>{
+    document.getElementById('abc').innerHTML="happy birthday butterfly";
+   },6000)
+
+        
+   
+        
+    
+        
+        
+     
+    
+}
+
+
+
+
+
